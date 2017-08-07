38a800d7c1f560520b46cd5c7a302ce4899f3537
Dave <startingline13@hotmail.com>
2017-08-07 19:54:21 -0400

    Removed extra packages

--- public/javascripts/imageManip.js
+++ public/javascripts/imageManip.js
@@ -1,6 +1,6 @@
 // save img: https://stackoverflow.com/questions/923885/capture-html-canvas-as-gif-jpg-png-pdf
 
-const getContext = () => document.getElementById('canvas').getContext('2d');
+const getContext = ()  => document.getElementById('canvas').getContext('2d');
 
 // It's better to use async image loading.
 const loadImage = url => {
@@ -25,6 +25,7 @@ const depict = options => {
 };
 
 // Must set coors on blobs to allow this to work.
+// Only increment Y value to offset images
 // SEE: Tainted canvas - https://stackoverflow.com/questions/22710627/tainted-canvases-may-not-be-exported
 const imgs = [
   { uri: 'images/mj.jpg',  x: 15, y:  15, sw: 50, sh: 50 },
@@ -40,7 +41,7 @@ imgs.forEach(depict);
 function saveCanvasToImg(){
     var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); //Convert image to 'octet-stream' (Just a download, really)
     console.log(image);
-    sindow.location.href = image;
+    window.location.href = image;
 }
 
 /** Saves image and downloads 
